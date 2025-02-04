c243b7199a7e81a426ed03c6d09e04cadd661f14
aboutphilippe <philippemarketing@gmail.com>
2025-02-05 00:04:24 +0100

    cleanup outdated

--- nextjs-together-llamaindex/backend/src/functions/llamaindex/utils/llamaIndexTogetherClient.ts
+++
@@ -1,9 +0,0 @@
-import { TogetherLLM, Settings } from "llamaindex";
-
-export function llamaIndexTogetherClient({ model }: { model: TogetherLLM["model"] }) {
-    Settings.llm = new TogetherLLM({
-        apiKey: process.env.TOGETHER_API_KEY,
-        model: model,
-    });
-    return Settings.llm;
-}
