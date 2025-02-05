fcd24b66d6948684ed80b650ff33f23669bc0cc5
aboutphilippe <philippemarketing@gmail.com>
2025-02-05 14:35:41 +0100

    add agent todo

---
+++ agent-todo/src/agents/agent.ts
@@ -0,0 +1,77 @@
+import {
+  defineEvent,
+  onEvent,
+  condition,
+  log,
+  step,
+} from "@restackio/ai/agent";
+import * as functions from "../functions";
+
+export type EndEvent = {
+  end: boolean;
+};
+
+export const messageEvent = defineEvent<functions.Message[]>("message");
+export const endEvent = defineEvent("end");
+
+type agentTodoOutput = {
+  messages: functions.Message[];
+};
+
+export async function agentTodo(): Promise<agentTodoOutput> {
+  let endReceived = false;
+  let messages: functions.Message[] = [];
+
+  const tools = await step<typeof functions>({}).getTools();
+
+  onEvent(messageEvent, async ({ content }: functions.Message) => {
+    messages.push({ role: "user", content: content?.toString() ?? "" });
+    const result = await step<typeof functions>({}).llmChat({
+      messages,
+      tools,
+    });
+
+    messages.push({ role: "assistant", content: result.content });
+
+    if (result.tool_calls) {
+      for (const toolCall of result.tool_calls) {
+        switch (toolCall.function.name) {
+          case "lookupSales":
+            const toolResult = await step<typeof functions>({}).lookupSales(
+              JSON.parse(toolCall.function.arguments)
+            );
+
+            messages.push({
+              role: "tool",
+              tool_call_id: toolCall.id,
+              content: JSON.stringify(toolResult),
+            });
+
+            const toolChatResult = await step<typeof functions>({}).llmChat({
+              messages,
+              tools,
+            });
+
+            messages.push({
+              role: "assistant",
+              content: toolChatResult.content,
+            });
+
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    return messages;
+  });
+
+  onEvent(endEvent, async () => {
+    endReceived = true;
+  });
+
+  await condition(() => endReceived);
+
+  log.info("end condition met");
+  return { messages };
+}
