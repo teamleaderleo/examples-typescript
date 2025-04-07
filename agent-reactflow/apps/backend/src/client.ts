de776d5139c31c46b1b38a9bc0b475433251a553
aboutphilippe <philippemarketing@gmail.com>
2025-04-07 13:53:38 +0530

    export api address

--- agent-reactflow/apps/backend/src/client.ts
+++ agent-reactflow/apps/backend/src/client.ts
@@ -1,12 +1,12 @@
 import Restack from "@restackio/ai";
 
-import "dotenv/config";
+export const apiAddress = process.env.RESTACK_ENGINE_API_ADDRESS!
 
 export const connectionOptions = {
   engineId: process.env.RESTACK_ENGINE_ID!,
   address: process.env.RESTACK_ENGINE_ADDRESS!,
   apiKey: process.env.RESTACK_ENGINE_API_KEY!,
-  apiAddress: process.env.RESTACK_ENGINE_API_ADDRESS!,
+  apiAddress,
 };
 
 export const client = new Restack(
